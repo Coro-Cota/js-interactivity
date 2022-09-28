70083d5ed1f1cf40602c82210cc68c45b5154184
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-09-28 13:25:23 -0700

    commit

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log("hello world");
+const message = document.querySelector('#message')
+
+
+function addMovie(event){
+    event.preventDefault();
+    const inputField = document.querySelector('input');
+    const movie = document.createElement('li');
+    const movieTitle = document.createElement ('span');
+    movieTitle.textContent = inputField.value;
+    addEventListener('click',crossOffMovie);
+    movie.appendChild(movieTitle);
+    const movieList = document.querySelector('ul');
+    movieList.appendChild(movie);
+    // delete button ▼▼▼▼▼
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X'
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn);
+    inputField.value=""
+    // delete button ^^^
+
+}
+
+function deleteMovie(event){
+    event.target.parentNode.remove()
+    message.textContent = 'Movie deleted!';
+}
+
+function crossOffMovie(event){
+    event.target.classList.toggle(`checked`);
+    if (event.target.classList.contains('checked')) {
+        message.textContent = 'Movie watched!'
+    } else {
+        message.textContent = 'Movie added back!'
+    }
+}
+
+const movieForm = document.querySelector('form')
+movieForm.addEventListener('submit', addMovie);
+
+
